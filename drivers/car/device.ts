503c5941da2a93dad96b5582c4238926ed184747
Adam Norén <norenz@hotmail.com>
2025-02-05 14:47:36 +0100

    Bump version to v1.1.0
    Changelog: Added more values and actions

--- drivers/car/device.ts
+++ drivers/car/device.ts
@@ -1,18 +1,23 @@
-import TessieApp from "../../TessieApp";
-import TessieDevice from "../TessieDevice";
-import tessie from "../../.api/apis/tessie";
+import Homey from "homey";
+import tessie, { GetStateResponse200 } from "../../.api/apis/tessie";
+import { Driver } from "homey";
 
-module.exports = class CarDevice extends TessieDevice {
+const POLL_INTERVAL = 30 * 1000; // 30 seconds
+
+module.exports = class CarDevice extends Homey.Device {
+  private timeout: NodeJS.Timeout | null = null;
+
   async onInit() {
-    await super.onInit();
-
-    // this.registerChargeListener();
-    // this.registerSetChargeCostAction();
-    // this.registerBatteryHealthTrigger();
+    // TODO
+    this.log("TessieDevice has been initialized");
+    await this._updateCapabilities();
+    await this._initActions();
+    await this._initConditions();
+    this.startPolling();
   }
 
-  async _auth() {
-    const accessToken = (this.homey.app as TessieApp).accessToken;
+  _auth() {
+    const accessToken = this.getData().accessToken;
 
     if (!accessToken) {
       throw new Error("Access token is missing");
@@ -21,115 +26,352 @@ module.exports = class CarDevice extends TessieDevice {
     tessie.auth(accessToken);
   }
 
-  async registerChargeListener() {
-    const vin = this.getData().id;
-    (this.homey.app as TessieApp).tessieApi?.onData(vin, (data) => async () => {
-      const lastChargeCreatedAt = this.getStoreValue("lastChargeCreatedAt");
+  async getState() {
+    try {
+      await this._auth();
+      const { data } = await tessie.getState({
+        vin: this.getData().id,
+      });
+      this.updateDevice(data);
+      return data;
+    } catch (error) {
+      this.error(`Failed to get state: ${error}`);
+    }
+  }
 
-      if (lastChargeCreatedAt !== data.last_charge_created_at) {
-        // Get the latest charge
+  async startPolling() {
+    await this.getState();
+    this.timeout = setInterval(() => {
+      this.getState();
+    }, POLL_INTERVAL);
+  }
 
-        const createdAt = data.last_charge_created_at;
-        const distance_format =
-          data.last_state?.gui_settings?.gui_distance_units === "km/hr"
-            ? "km"
-            : "mi";
+  // Stop polling for data.
+  async stopPolling() {
+    if (this.timeout) {
+      clearInterval(this.timeout);
+    }
+  }
 
-        const charges = await tessie.getCharges({
-          vin,
-          distance_format,
+  async _updateCapabilities() {
+    let capabilities = [];
+    try {
+      capabilities = this.homey.app.manifest.drivers.filter((e: Driver) => {
+        return e.id == this.driver.id;
+      })[0].capabilities;
+      // remove capabilities
+      let deviceCapabilities = this.getCapabilities();
+      for (let i = 0; i < deviceCapabilities.length; i++) {
+        let filter = capabilities.filter((e: string) => {
+          return e == deviceCapabilities[i];
         });
-
-        const lastCharge = charges.data.results?.[0];
-        const newChargeTrigger =
-          this.homey.flow.getDeviceTriggerCard("new_charge");
-
-        if (lastCharge && lastCharge.createdAt === createdAt) {
-          // Update the store
-          this.setStoreValue("lastChargeCreatedAt", createdAt);
-
-          // Trigger flow
-          newChargeTrigger.trigger(this, {
-            id: lastCharge.id,
-            started_at: lastCharge.started_at,
-            ended_at: lastCharge.ended_at,
-            created_at: lastCharge.created_at,
-            location: lastCharge.location,
-            saved_location: lastCharge.saved_location,
-            latitude: lastCharge.latitude,
-            longitude: lastCharge.longitude,
-            is_supercharger: lastCharge.is_supercharger,
-            is_fast_charger: lastCharge.is_fast_charger,
-            odometer: lastCharge.odometer,
-            energy_added: lastCharge.energy_added,
-            energy_used: lastCharge.energy_used,
-            miles_added: lastCharge.miles_added,
-            miles_added_ideal: lastCharge.miles_added_ideal,
-            starting_battery: lastCharge.starting_battery,
-            ending_battery: lastCharge.ending_battery,
-            cost: lastCharge.cost,
-          });
+        if (filter.length == 0) {
+          try {
+            await this.removeCapability(deviceCapabilities[i]);
+          } catch (error) {}
         }
       }
-    });
+      // add missing capabilities
+      for (let i = 0; i < capabilities.length; i++) {
+        if (!this.hasCapability(capabilities[i])) {
+          try {
+            await this.addCapability(capabilities[i]);
+          } catch (error) {}
+        }
+      }
+    } catch (error) {
+      this.error(error);
+    }
   }
 
-  async registerSetChargeCostAction() {
-    const action = this.homey.flow.getActionCard("set_charge_cost");
-    action.registerRunListener(async (args, state) => {
-      const vin = this.getData().id;
-      const { id, cost } = args;
-
-      const accessToken = (this.homey.app as TessieApp).accessToken;
-
-      if (!accessToken) {
-        throw new Error("Access token is missing");
-      }
-
-      tessie.auth(accessToken);
-      await tessie.setChargeCost({
-        vin,
-        id,
-        cost,
-      });
-    });
+  async sendCommand(command: () => Promise<void>) {
+    this._auth();
+    return await command();
   }
 
-  async registerBatteryHealthTrigger() {
-    const action = this.homey.flow.getActionCard("get_battery_health");
-    action.registerRunListener(async (args, state) => {
-      const vin = this.getData().id;
-      const accessToken = (this.homey.app as TessieApp).accessToken;
-
-      if (!accessToken) {
-        throw new Error("Access token is missing");
-      }
-
-      tessie.auth(accessToken);
-
-      const distance_format = this.getSettings().distance_format;
-
-      const { data } = await tessie.getBatteryHealth({
-        distance_format,
-      });
-
-      // Get the latest battery health that has vin matching the device
-      const batteryHealth = data.results?.find((result) => result.vin === vin);
-
-      // Replace any null values with empty strings
-      if (batteryHealth) {
-        Object.keys(batteryHealth).forEach((key) => {
-          if (batteryHealth[key] === null) {
-            batteryHealth[key] = "";
-          }
+  async _initActions() {
+    this.homey.flow
+      .getActionCard("charge_current")
+      .registerRunListener(async (args, state) => {
+        this.sendCommand(async () => {
+          await tessie.setChargingAmps({
+            amps: args.current,
+            vin: this.getData().id,
+            wait_for_completion: true,
+          });
+          await this.setCapabilityValue(
+            "measure_charge_current_max",
+            args.current
+          );
         });
-      }
+      });
 
-      if (batteryHealth) {
-        return batteryHealth;
-      } else {
-        throw new Error("Battery health not found");
-      }
-    });
+    this.homey.flow
+      .getActionCard("charging_on")
+      .registerRunListener(async (args, state) => {
+        this.sendCommand(async () => {
+          if (args.action === "start") {
+            this.log("Starting charging");
+            await tessie.startCharging({
+              vin: this.getData().id,
+              wait_for_completion: true,
+            });
+          } else {
+            this.log("Stopping charging");
+            await tessie.stopCharging({
+              vin: this.getData().id,
+              wait_for_completion: true,
+            });
+          }
+          await this.setCapabilityValue("charging_on", args.action === "start");
+        });
+      });
+
+    this.homey.flow
+      .getActionCard("charge_limit")
+      .registerRunListener(async (args, state) => {
+        this.sendCommand(async () => {
+          this.log("Setting charge limit: ", args.limit);
+          await tessie.setChargeLimit({
+            percent: args.limit,
+            vin: this.getData().id,
+            wait_for_completion: true,
+          });
+          await this.setCapabilityValue("measure_charge_limit_soc", args.limit);
+        });
+      });
+  }
+
+  async _initConditions() {
+    this.homey.flow
+      .getConditionCard("charging_state")
+      .registerRunListener(async (args, state) => {
+        if (args.state == "Connected") {
+          return (
+            args.device.getCapabilityValue("charging_state") != "Disconnected"
+          );
+        } else {
+          return args.device.getCapabilityValue("charging_state") == args.state;
+        }
+      });
+  }
+
+  async updateDevice(data: GetStateResponse200) {
+    if (
+      this.hasCapability("measure_battery") &&
+      data.charge_state?.battery_level
+    ) {
+      await this.setCapabilityValue(
+        "measure_battery",
+        data.charge_state.battery_level
+      );
+    }
+
+    if (this.hasCapability("meter_car_odo") && data.vehicle_state?.odometer) {
+      await this.setCapabilityValue(
+        "meter_car_odo",
+        data.vehicle_state.odometer
+      );
+    }
+
+    if (
+      this.hasCapability("charging_on") &&
+      data.charge_state?.charging_state
+    ) {
+      await this.setCapabilityValue(
+        "charging_on",
+        data.charge_state.charging_state === "Charging"
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_current_max") &&
+      data.charge_state?.charge_amps
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_current_max",
+        data.charge_state.charge_amps
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_current") &&
+      data.charge_state?.charger_actual_current
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_current",
+        data.charge_state.charger_actual_current
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_energy_added") &&
+      data.charge_state?.charge_energy_added
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_energy_added",
+        data.charge_state.charge_energy_added
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_limit_soc") &&
+      data.charge_state?.charge_limit_soc
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_limit_soc",
+        data.charge_state.charge_limit_soc
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_minutes_to_full_charge") &&
+      data.charge_state?.minutes_to_full_charge
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_minutes_to_full_charge",
+        data.charge_state.minutes_to_full_charge
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_phases") &&
+      data.charge_state?.charger_phases
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_phases",
+        data.charge_state.charger_phases
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_power") &&
+      data.charge_state?.charger_power
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_power",
+        data.charge_state.charger_power
+      );
+    }
+
+    if (
+      this.hasCapability("measure_charge_voltage") &&
+      data.charge_state?.charger_voltage
+    ) {
+      await this.setCapabilityValue(
+        "measure_charge_voltage",
+        data.charge_state.charger_voltage
+      );
+    }
+
+    if (
+      this.hasCapability("measure_climate_temperature_in") &&
+      data.climate_state?.inside_temp
+    ) {
+      await this.setCapabilityValue(
+        "measure_climate_temperature_in",
+        data.climate_state.inside_temp
+      );
+    }
+
+    if (
+      this.hasCapability("measure_climate_temperature_out") &&
+      data.climate_state?.outside_temp
+    ) {
+      await this.setCapabilityValue(
+        "measure_climate_temperature_out",
+        data.climate_state.outside_temp
+      );
+    }
+
+    if (
+      this.hasCapability("measure_io_battery_power") &&
+      data.drive_state?.power
+    ) {
+      await this.setCapabilityValue(
+        "measure_io_battery_power",
+        data.drive_state.power
+      );
+    }
+
+    if (
+      this.hasCapability("measure_location_heading") &&
+      data.drive_state?.heading
+    ) {
+      await this.setCapabilityValue(
+        "measure_location_heading",
+        data.drive_state.heading
+      );
+    }
+
+    if (
+      this.hasCapability("measure_location_latitude") &&
+      data.drive_state?.latitude
+    ) {
+      await this.setCapabilityValue(
+        "measure_location_latitude",
+        data.drive_state.latitude
+      );
+    }
+
+    if (
+      this.hasCapability("measure_location_longitude") &&
+      data.drive_state?.longitude
+    ) {
+      await this.setCapabilityValue(
+        "measure_location_longitude",
+        data.drive_state.longitude
+      );
+    }
+
+    if (
+      this.hasCapability("measure_soc_level") &&
+      data.charge_state?.battery_level
+    ) {
+      await this.setCapabilityValue(
+        "measure_soc_level",
+        data.charge_state.battery_level
+      );
+    }
+
+    if (
+      this.hasCapability("measure_soc_range_estimated") &&
+      data.charge_state?.est_battery_range
+    ) {
+      await this.setCapabilityValue(
+        "measure_soc_range_estimated",
+        data.charge_state.est_battery_range
+      );
+    }
+
+    if (
+      this.hasCapability("measure_soc_range_ideal") &&
+      data.charge_state?.ideal_battery_range
+    ) {
+      await this.setCapabilityValue(
+        "measure_soc_range_ideal",
+        data.charge_state.ideal_battery_range
+      );
+    }
+
+    if (
+      this.hasCapability("measure_soc_usable") &&
+      data.charge_state?.usable_battery_level
+    ) {
+      await this.setCapabilityValue(
+        "measure_soc_usable",
+        data.charge_state.usable_battery_level
+      );
+    }
+
+    if (
+      this.hasCapability("measure_battery") &&
+      data.charge_state?.battery_level
+    ) {
+      await this.setCapabilityValue(
+        "measure_battery",
+        data.charge_state.battery_level
+      );
+    }
   }
 };
